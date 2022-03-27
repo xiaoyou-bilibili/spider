6e616ca8ba482a5b14d7b3156bdfa57065b41c37
Liam Sho <conan_creeper@outlook.com>
2022-03-27 22:41:11 +0800

    优化 C# 版本
    1. 使用 HttpClient，WebRequest 已被标记为 Obsolete
    2. 采用异步方法
    3. 优化结构以符合代码规范

--- c#/Program.cs
+++ c#/Program.cs
@@ -1,78 +1,93 @@
 ﻿// 使用系统提供的各种包
-using System;
-using System.Net;
-using System.IO;
-using System.Text.RegularExpressions;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace spider
 {
-    class Program
+    public class Program
     {
+        private readonly static HttpClient _client = new();
+
         // 发送get请求获取内容
-        public static string Get(string url){
-            string result="";
+        public async static Task<string?> GetAsync(string url)
+        {
             // 发送一个request请求
-            WebRequest request = WebRequest.Create(url);
-            WebResponse resp = request.GetResponse();
-            Stream stream=resp.GetResponseStream();
+            var resp = await _client.GetAsync(url);
+            // 失败时返回空
+            if (resp.IsSuccessStatusCode is false)
+            {
+                return null;
+            }
             // 注册扩展包(支持GBK编码)
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            try{
-                using(var memoryStream = new MemoryStream())
-                {
-                    // 直接把stram拷贝到MemoryStream
-                    // https://stackoverflow.com/questions/1080442/how-to-convert-an-stream-into-a-byte-in-c
-                    stream.CopyTo(memoryStream);
-                    byte[] buffer =  memoryStream.ToArray();
-                    // 这里我们需要提前使用GBK来进行解码，如果是UTF8可以使用Encoding.UTF8
-                    result = Encoding.GetEncoding("GBK").GetString(buffer, 0, buffer.Length);
-                }
-            }finally{
-                stream.Close();
-            }
+            using var memoryStream = new MemoryStream();
+            // 直接把stram拷贝到MemoryStream
+            // https://stackoverflow.com/questions/1080442/how-to-convert-an-stream-into-a-byte-in-c
+            await resp.Content.CopyToAsync(memoryStream);
+            var buffer = memoryStream.ToArray();
+            // 这里我们需要提前使用GBK来进行解码，如果是 UTF8 可以使用 Encoding.UTF8
+            var result = Encoding.GetEncoding("GBK").GetString(buffer, 0, buffer.Length);
             return result;
         }
-        
+
         // 获取某一个章节的内容
-        public static string Getcontent(string url){
-            string content=Get(url);
-            string result="";
+        public async static Task<string?> GetContentAsync(string url)
+        {
+            var content = await GetAsync(url);
+            if (content is null)
+            {
+                return null;
+            }
+            var result = "";
             //遍历我们查找的的结果
-            foreach(Match match in Regex.Matches(content,"showtxt\">([\\s\\S]*?)</div>"))
-                result+=match.Groups[1].Value;
+            foreach (Match match in Regex.Matches(content, "showtxt\">([\\s\\S]*?)</div>"))
+            {
+                result += match.Groups[1].Value;
+            }
             // 替换掉无关的内容
-            result = result.Replace("&nbsp;","");
-            result = result.Replace("<br />","\n");
+            result = result.Replace("&nbsp;", "").Replace("<br />", "\n");
             return result;
         }
 
         // 获取所有章节信息
-        public static void GetAllCapter(){
-            string content=Get("http://www.qiushuge.net/zhongjidouluo/");
-            string parrent1="<dd><a href =\"(.*?)\"";
-            string parrent2="\">(.*?)</a></dd>";
+        public async static Task GetAllCapterAsync()
+        {
+            var chapters = await GetAsync("http://www.qiushuge.net/zhongjidouluo/");
+            if (chapters is null)
+            {
+                Console.Error.WriteLine("获取失败，Chapters 为空");
+                return;
+            }
+            var parrent1 = "<dd><a href =\"(.*?)\"";
+            var parrent2 = "\">(.*?)</a></dd>";
             // 使用正则匹配一下内容
-            MatchCollection links=Regex.Matches(content,parrent1);
-            MatchCollection names=Regex.Matches(content,parrent2);
+            var links = Regex.Matches(chapters, parrent1);
+            var names = Regex.Matches(chapters, parrent2);
             // 打开一个文件
-            FileStream fileStream=new FileStream("content.txt",FileMode.Append);
-            StreamWriter sw=new StreamWriter(fileStream);
-            for(int i=0;i<links.Count;i++){
-                sw.Write(names[i].Groups[1].Value+"\n\n");
-                Console.WriteLine("正在下载"+names[i].Groups[1].Value);
-                sw.Write(Getcontent("http://www.qiushuge.net"+links[i].Groups[1].Value));
+            var fileStream = new FileStream("content.txt", FileMode.Append);
+            var sw = new StreamWriter(fileStream);
+            for (int i = 0; i < links.Count; i++)
+            {
+                await sw.WriteAsync(names[i].Groups[1].Value + "\n\n");
+                Console.WriteLine("正在下载" + names[i].Groups[1].Value);
+                var contentUrl = "http://www.qiushuge.net" + links[i].Groups[1].Value;
+                var content = await GetContentAsync(contentUrl);
+                if (content is null)
+                {
+                    Console.Error.WriteLine($"获取失败，Content 为空，URL = {contentUrl}");
+                    continue;
+                }
+                await sw.WriteLineAsync(content);
             }
             // 写入文件信息
-            sw.Flush();
             sw.Close();
             fileStream.Close();
         }
 
         // 主函数入口
-        static void Main(string[] args)
+        public async static Task Main()
         {
-            GetAllCapter();
+            await GetAllCapterAsync();
         }
     }
 }
